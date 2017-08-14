d5cf9ddb967fe7f372f3580b520ab2f444ecefa5
breno-helf <breno.moura@hotmail.com>
2017-08-14 17:19:54 -0300

    Parte 1 finalizada

--- EP1/ep1sh.c
+++ EP1/ep1sh.c
@@ -55,17 +55,15 @@ void make_command(char * str, char ** command) {
     command[number] = NULL;
 }
 
+/*
+  Change the owner group based on
+  chown system command
+*/
 void change_owner_group(char *gr, char *file) {
     char * dir;
 
-    printf("--> %d\n", (int)strlen(file));
-    printf("--> %s\n", gr + 1);
-    
     dir = (char *) malloc(sizeof(char) * MAX);
-   
-    printf("Malloquei\n");
-    fflush(stdout);
-	    
+   	    
     getcwd(dir, MAX - (strlen(file) + 2));
     strcat(dir, "/");
     strcat(dir, file);
@@ -73,10 +71,24 @@ void change_owner_group(char *gr, char *file) {
     struct group * g = getgrnam(gr + 1);
     
     gid_t GroupID = g->gr_gid;
-	    
-    if (chown(dir, -1, GroupID) == 0)
+    
+    if (chown(file, -1, GroupID) == -1)
 	fprintf(stderr, "chown failed: %s\n", strerror(errno));
     free(dir);
+}
+
+void execute_command(char * dir, char * args[]) { 
+    pid_t pid;
+    int status;
+    if ((pid = fork()) == 0) {
+	if (execv(dir, args) == -1)
+	    fprintf(stderr, "%s failed: %s\n", dir, strerror(errno));
+	
+	status = 0;
+	exit(0);
+    } else {
+	waitpid(pid, &status, 0);
+    }
 }
 
 int main(int argc, char * argv[]) {
@@ -96,7 +108,7 @@ int main(int argc, char * argv[]) {
 	} else if (args_num(str) != 0) add_history(str);
 	else continue;
 	
-	command = (char **) malloc(sizeof(char *) * args_num(str));
+	command = (char **) malloc(sizeof(char *) * (args_num(str) + 1));
 	make_command(str, command);
 	
 	if (strcmp(command[0], "exit") == 0) {
@@ -107,6 +119,35 @@ int main(int argc, char * argv[]) {
 	      chown :mysql in
 	    */
 	    change_owner_group(command[1], command[2]);
+	} else if (strcmp(command[0], "date") == 0) {
+	    /*
+	      Imprimir o dia
+	      de hoje
+	    */	    
+	    time_t * now = (time_t *) malloc(sizeof(time_t));
+	    time(now);
+	    printf("%s", ctime(now));
+	} else if (strcmp(command[0], "/bin/ping") == 0) {
+	    /*
+	      Mandar pacotes para o
+	      link desejado
+	    */
+	    
+	    execute_command(command[0], command);	    
+	} else if (strcmp(command[0], "/usr/bin/cal") == 0) {
+	    /*
+	      Imprimir calendário
+	      do ano dado
+	    */
+	    execute_command(command[0], command);	    
+	} else if (strcmp(command[0], "./ep1") == 0) {
+	    /*
+	      Rodar o simulador de 
+	      processos
+	    */
+	    execute_command(command[0], command);	    
+	} else {
+	    fprintf(stderr, "Command %s not supported\n", str);
 	}
 	
 	free(str);
