427be9c9fbd82d3e6a3efb5a965634a7c438cdcf
lucas-daher <lucas.daher@usp.br>
2017-08-30 01:57:52 -0300

    script

---
+++ EP1/testes/runner.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+for trace in range(1, 4):    
+    for escal in range(1, 4):
+        for i in range(1, 31):
+            print("Executando teste %d do trace %d com escalonador %d" % (i, trace, escal))
+        os.system("./ep1 %d arquivoTrace%d.txt OutTrace%descal%dt%d.txt" % (escal, trace, trace, escal, i) )
