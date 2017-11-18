e948a4b50648c5b0ce9b0855fbeba1fab965ab8c
lucas-daher <lucas.daher@usp.br>
2017-11-18 19:29:50 -0200

    sem segfault

--- EP3/ep3.cpp
+++ EP3/ep3.cpp
@@ -139,7 +139,14 @@ void simulate(trace * T, int mem_type, int pag_type, int print_time) {
 	    if (A.type == 1) {
 		// Inicializa um processo
 		process cur_process = T->process_vec[A.process_id];
-		M->insert(A.process_id, cur_process.b);
+		if (!M->insert(A.process_id, cur_process.b)) {
+            M->compact(pageMap);
+            P->compact(pageMap);
+            if (!M->insert(A.process_id, cur_process.b)) {
+                cerr << "nao ha memoria suficiente para simular\n";
+                return;
+            }
+        }
 	    } else if (A.type == 2) {
 		// Acessa a memória na paginação
 		process cur_process = T->process_vec[A.process_id];
