b534639d3853c77b13029d62058682fe792db22f
breno-helf <breno.moura@hotmail.com>
2017-11-18 19:00:47 -0200

    novo trace file

--- EP3/testes/tracegen.py
+++ EP3/testes/tracegen.py
@@ -4,14 +4,14 @@ from random import shuffle
 END_TIME = 300
 START_TIME = 0
 
-total = 40
-virtual = 80
+total = 60
+virtual = 120
 s = 8
 p = 4
 print(str(total) + " " + str(virtual) + " " + str(s) + " " + str(p))
 
-process_num = 10
-compact_num = randint(1, 5)
+process_num = 3
+compact_num = randint(1, 3)
 
 v = []
 
@@ -22,7 +22,7 @@ for i in range(compact_num):
 for i in range(process_num):
     t0 = randint(START_TIME, END_TIME)
     tf = randint(t0, END_TIME)
-    b  = randint(1, 40)
+    b  = randint(1, 30)
     nome = "Processo" + str(i)
     acessos = randint(1, 10)
     s = (str(t0) + " " + str(tf) + " " + str(b) + " " + nome)
